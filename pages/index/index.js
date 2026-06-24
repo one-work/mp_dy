2ee6dec150e00f68329fc39463ecc8955e4f8c1b
qinmingyuan <mingyuan0715@foxmail.com>
2026-06-24 14:24:25 +0800

    fix

--- pages/index/index.js
+++ pages/index/index.js
@@ -2,7 +2,7 @@ const app = getApp()
 
 Page({
   data: {
-    url: 'https://cms.one.work/eventual/events'
+    url: 'https://shu.ren/bench/facilitates'
   },
   onLoad: function () {
     console.log('Welcome to Mini Code')
