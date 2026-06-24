34b15509bd530905c3892c25055fd27a41faea70
qinmingyuan <mingyuan0715@foxmail.com>
2026-06-24 16:46:15 +0800

    pages logic

---
+++ pages/login/index.js
@@ -0,0 +1,46 @@
+const AUTH_HOST = 'https://shu.ren'
+const APPID = tt.getEnvInfoSync().microapp.appId
+
+Page({
+  onLoad(query) {
+    console.debug('login onLoad query:', query)
+
+    tt.login({
+      success: res => {
+        tt.request({
+          url: AUTH_HOST + '/douyin/douyin_users',
+          method: 'POST',
+          header: {
+            Accept: 'application/json'
+          },
+          data: {
+            code: res.code,
+            appid: APPID,
+            ...query
+          },
+          success: res => {
+            tt.redirectTo({
+              url: `/pages/index/index?url=${encodeURIComponent(res.data.url)}`
+            })
+          },
+          fail: res => {
+            let content = JSON.stringify(res)
+            if (res.errno === 600002) {
+              content = `${res.errMsg}：${AUTH_HOST}`
+            }
+            tt.showModal({
+              title: `Login page login request fail`,
+              content: content
+            })
+          }
+        })
+      },
+      fail: res => {
+        tt.showModal({
+          title: 'wx.login fail',
+          content: JSON.stringify(res)
+        })
+      }
+    })
+  }
+})
--- pages/web/index.js
+++ pages/web/index.js
@@ -1,11 +1,19 @@
 const app = getApp()
+const HOST = 'https://shu.ren'
+
 
 Page({
   data: {
     url: 'https://shu.ren/bench/facilitates'
   },
 
-  onLoad: function () {
+  onLoad() {
     console.log('Welcome to Mini Code')
   },
+
+  onMessage(options) {
+    console.debug(options)
+    this.login()
+  }
+
 })
